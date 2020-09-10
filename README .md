6f2c80f4bc2683aad773a8f3831293fa79650f3c
Bryner Pereira <brynnersb@gmail.com>
2020-09-10 09:34:32 -0300

    Desafio 2-3 Concluido

---
+++ README .md
@@ -0,0 +1,7 @@
+#Desafio LaunchBase 2-3 
+
+Criando uma página de portifólio usando modal, controlando com JavaScript.
+
+## :memo: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](/LICENSE) para mais detalhes.
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,12 +1,25 @@
 const modalOverlay = document.querySelector('.modal-overlay')
 const cards = document.querySelectorAll ('.card')
+const maximize = document.querySelector ('.modal')
 
 for ( let card of cards){
     card.addEventListener("click", function(){
         modalOverlay.classList.add('active')
+        const cardId = card.getAttribute("id")
+        modalOverlay.querySelector("iframe").src = `https://rocketseat.com.br/${cardId}`
     })
 } 
 
 document.querySelector(".close-modal").addEventListener("click", function () {
     modalOverlay.classList.remove('active')
+})
+
+document.querySelector(".maximize-modal").addEventListener("click", function(){
+
+        if (maximize.classList.contains('maximize') === false){
+            maximize.classList.add("maximize")
+        }else{
+            maximize.classList.remove("maximize")
+        }
+
 })
